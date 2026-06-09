276f89fa330e9d57140863e277d3672e4df9c38b
Juan  Pablo Sanchez Fragoso <68075796+juanp611@users.noreply.github.com>
2026-06-09 09:40:09 -0500

    Update DropZone.cs
    Ajustes en la descripción

--- Script/DropZone.cs
+++ Script/DropZone.cs
@@ -8,7 +8,7 @@ public class DropZone : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         RectTransform droppedTask = eventData.pointerDrag.GetComponent<RectTransform>();
-        droppedTask.SetParent(transform); // Mover la tarjeta al nuevo padre (la nueva columna)
+        droppedTask.SetParent(transform); // Mover la tarjeta a la nueva columna
         droppedTask.anchoredPosition = Vector3.zero; // Posicionarla correctamente en la columna
     }
 }
